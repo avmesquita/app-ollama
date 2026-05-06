114108bbbba14c47e20541df9a946324de5008d2
Andre Mesquita <avmesquita@gmail.com>
2026-05-06 08:20:53 -0300

    First basic example

---
+++ app-ollama/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient chatClient = new OllamaApiClient(
+    new Uri("http://hp-envy-17:11434"),
+    "qwen2.5-coder:1.5b");
+
+List<ChatMessage> chatHistory = new();
+
+while (true)
+{
+    Console.Write("User: ");
+    string userInput = Console.ReadLine() ?? string.Empty;
+
+    chatHistory.Add(new ChatMessage(ChatRole.User, userInput));
+
+    Console.WriteLine($"Assistant: ");
+    var assistantResponse = "";
+
+    await foreach(var update in chatClient.GetStreamingResponseAsync(chatHistory))
+    {
+        Console.Write(update.Text);
+        assistantResponse += update.Text;
+    }
+
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, assistantResponse));
+    Console.WriteLine("\n\n");
+}
